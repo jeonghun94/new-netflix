33abf04061529093253475afb8b53e3d430be4ea
조정훈 <jeongh1021@naver.com>
2023-03-03 13:40:42 +0900

    gh pages test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+
 function App() {
-  return <div>hi</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/about" element={<h1>About</h1>} />
+    </Routes>
+  );
 }
 
 export default App;
